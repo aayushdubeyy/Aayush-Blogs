59c7aef0a970160ce1e27b47bee3471c0160a5a8
aayush dubeyy <aayushdubey040@gmail.com>
2023-09-08 23:18:29 +0530

    Blogs Initial Commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full border-b-4 shadow-md py-4 fixed top-0 bg-white'>
+      <header className='text-center'>
+        <h1 className='text-3xl font-bold uppercase'>Aayush Blogs</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
